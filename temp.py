10ca6ca2f0cd8689a34b1bca73da2a2953d8360a
jihe5893 <jihe5893@uni.sydney.edu.au>
2022-12-29 16:06:16 +1100

    updated result

--- temp.py
+++ temp.py
@@ -1,4 +1,6 @@
 
+python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 100 --dim 80 --local_epochs 30 --ro 1 --dataset Elec20 --window 80 --ro_auto 1
+python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 100 --dim 160 --local_epochs 30 --ro 1 --dataset Elec20 --window 160 --ro_auto 1
 
 python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 100 --dim 80 --local_epochs 30 --ro 1 --dataset Elec370 --window 80 --ro_auto 1
 python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 100 --dim 160 --local_epochs 30 --ro 1 --dataset Elec370 --window 160 --ro_auto 1

0df0adc1a9fb2ae430837b5d1e7bfaf1dfe8695e
TungAnh <tunganhbgbk@gmail.com>
2023-04-11 14:25:31 +1000

    Add experiments for 18 users Elec370 and 860 users Traff860

--- README.md
+++ README.md
@@ -41,6 +41,8 @@ python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 50 --
 
 python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 50 --dim 80 --local_epochs 30 --ro 1 --dataset Elec370 --window 80 --ro_auto 1 --missingVal 80 --mulTS 1 --fac 1 --num_users 10
 
+python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 50 --dim 80 --local_epochs 30 --ro 1 --dataset Elec370 --window 80 --ro_auto 1 --missingVal 0 --mulTS 1 --fac 1 --num_users 18
+
 python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 50 --dim 80 --local_epochs 30 --ro 1 --dataset Elec370 --window 80 --ro_auto 1 --missingVal 0 --mulTS 1 --fac 1 --num_users 37
 
 python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 50 --dim 80 --local_epochs 30 --ro 1 --dataset Elec370 --window 80 --ro_auto 1 --missingVal 20 --mulTS 1 --fac 1 --num_users 37
@@ -135,11 +137,13 @@ python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 5 --d
 python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 5 --dim 100 --local_epochs 10 --ro 1 --dataset Traffic860 --window 100 --ro_auto 1 --missingVal 60 --mulTS 1 --fac 1 --num_users 430
 
 python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 5 --dim 100 --local_epochs 10 --ro 1 --dataset Traffic860 --window 100 --ro_auto 1 --missingVal 80 --mulTS 1 --fac 1 --num_users 430
+
+python3 main.py --algorithm FedPG --learning_rate 0.005 --num_global_iters 5 --dim 100 --local_epochs 10 --ro 1 --dataset Traffic860 --window 100 --ro_auto 1 --missingVal 0 --mulTS 1 --fac 1 --num_users 860
+
 <code></pre>
 
 ## Running command for FedLSTM with missing data in multivariate-time series on client with Elec370 dataset
 <pre></code>
-python3 main.py --dataset Imputed_Elec370 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.2 --num_global_iters 100 --local_epochs 2 --num_users 10 --mulTS 1 --missingVal 0 --datatype hankel
 
 python3 main.py --dataset Imputed_Elec370 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 0 --datatype hankel
 
@@ -152,6 +156,8 @@ python3 main.py --dataset Imputed_Elec370 --algorithm FedLSTM --batch_size 64 --
 python3 main.py --dataset Imputed_Elec370 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.2 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 60 --datatype hankel
 
 python3 main.py --dataset Imputed_Elec370 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.2 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 80 --datatype hankel
+
+python3 main.py --dataset Imputed_Elec370 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 18 --mulTS 1 --missingVal 0 --datatype hankel
 
 python3 main.py --dataset Imputed_Elec370 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 37 --mulTS 1 --missingVal 0 --datatype hankel
 
@@ -252,6 +258,11 @@ python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 -
 python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 60 --datatype hankel
 
 python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 80 --datatype hankel
+
+python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 860 --mulTS 1 --missingVal 0 --datatype hankel
+
+
+
 <code></pre>
 
 ## Running command for FedLR with no-missing data in multivariate-time series on client
--- automated_script.sh
+++ automated_script.sh
@@ -1,55 +1,57 @@
 #!/bin/bash
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 0 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 0 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 20 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 20 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 40 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 40 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 60 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 60 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 80 --datatype hankel
-
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 0 --datatype hankel
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 20 --datatype hankel
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 40 --datatype hankel
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 60 --datatype hankel
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 80 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 10 --mulTS 1 --missingVal 80 --datatype hankel
 
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 0 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 0 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 20 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 20 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 40 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 40 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 60 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 60 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 80 --datatype hankel
-
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 0 --datatype hankel
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 20 --datatype hankel
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 40 --datatype hankel
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 60 --datatype hankel
-
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 80 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 43 --mulTS 1 --missingVal 80 --datatype hankel
 
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 0 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 0 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 20 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 20 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 40 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 40 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 60 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 60 --datatype hankel
 
-python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 80 --datatype hankel
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 86 --mulTS 1 --missingVal 80 --datatype hankel
+
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 0 --datatype hankel
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 20 --datatype hankel
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 40 --datatype hankel
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 60 --datatype hankel
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 172 --mulTS 1 --missingVal 80 --datatype hankel
+
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 0 --datatype hankel
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 20 --datatype hankel
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 40 --datatype hankel
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 60 --datatype hankel
+
+# python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 430 --mulTS 1 --missingVal 80 --datatype hankel
+
+python3 main.py --dataset Imputed_Traff860 --algorithm FedLSTM --batch_size 64 --learning_rate 0.001 --fac 0.3 --num_global_iters 20 --local_epochs 1 --num_users 860 --mulTS 1 --missingVal 0 --datatype hankel
